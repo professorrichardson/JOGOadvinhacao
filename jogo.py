469ec1e2e6fb1fcd6971099516365ff81fd94afc
Richardson Schawarski Cruz <125823124+professorrichardson@users.noreply.github.com>
2025-07-31 12:17:48 +0000

    primeira parte do jogo

---
+++ jogo.py
@@ -0,0 +1,9 @@
+import random
+print("###############################")
+print("#  Jogo de adivinhação        #")
+print("#     Richardson              #")
+print("###############################")
+
+numeroSecreto= random.randrange(0,100)
+totalTentativas = 0
+pontos = 100
